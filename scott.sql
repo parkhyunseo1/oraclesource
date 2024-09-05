f7898e72a3edafdcb0cf1368969d5cc93f929b4e
박현서 <qkr2625@gmail.com>
2024-09-05 17:57:50 +0900

    2024 09 05 sql

--- scott.sql
+++ scott.sql
@@ -203,8 +203,281 @@ INTERSECT
 SELECT E.EMPNO ,E.ENAME ,E.SAL ,E.DEPTNO 
 FROM  EMP e 
 WHERE E.DEPTNO = 10; 
+
+-- ######################
+-- 함수
+-- 1. 오라클 내장 함수
+-- 2. 사용자 정의 함수(PL/SQL)
  
- 
+-- 오라클 내장 함수
+-- 1. 단일행 함수 : 데이터가 한 행 씩 입력되고 입력된 한 행당 결과가 하나씩 나오는 함수
+-- 2. 다중행 함수 : 여러 행이 입력되고 결과가 하나의 행으로 반환되는 함수
+
+-- 1. 문자함수 
+-- UPPER(문자열) : 괄호 안 문자열을 모두 대문자로
+-- LOWER(문자열) : 괄호 안 문자열을 모두 소문자로
+-- INITCAP(문자열) : 괄호 안 문자 데이터 중 첫문자만 대문자, 나머지는 소문자로 
+-- LENGTH(문자열) : 문자열 길이
+-- LENGTHB(문자열) : 문자열 바이트 수
+-- SUBSTR(문자열,시작위치) / SUBSTR(문자열,시작위치,추출길이)
+-- INSTR(문자열, 찾으려는문자) : 특정 문자나 문자열이 어디에 포함되어 있는지
+-- INSTR(문자열,찾으려는문자,위치 찾기를 시작할 대상 문자열 데이터 위치, 시작위치에서 찾으려는 문자가 몇 번째인지)
+-- REPLACE(문자열, 찾는문자, 대체문자)
+-- CONCAT(문자열1, 문자열2)
+-- TRIM / LTRIM / RTRIM : 특정 문자를 제거
+
+-- 데이터는 대소문자 구분함
+-- ORACLE = oracle 틀림
+SELECT E.ENAME, UPPER(E.ENAME),LOWER(E.ENAME), INITCAP(E.ENAME) 
+FROM EMP e ;
+
+SELECT * FROM EMP e ;
+
+--smith
+SELECT *
+FROM EMP e 
+WHERE UPPER(ENAME) = UPPER('smith');
+
+SELECT *	
+FROM EMP e 
+WHERE UPPER(ENAME) LIKE UPPER('%smith%'); 
+
+-- DUAL : 오라클에서 제공하는 기본 테이블(함수 적용 결과)
+-- LENGTH / LENGTHB
+SELECT LENGTH('한글'), LENGTHB('한글'), LENGTH('AB'), LENGTHB('AB')
+FROM DUAL;
+
+SELECT JOB,SUBSTR(JOB,1,2),SUBSTR(JOB,3,2),SUBSTR(JOB,5) 
+FROM EMP e ;
+
+-- - : 오른쪽 끝을 의미
+SELECT JOB,SUBSTR(JOB,-LENGTH(JOB)),SUBSTR(JOB,-LENGTH(JOB),2),SUBSTR(JOB,3)
+FROM EMP e ;
+
+SELECT
+	INSTR('HELLO, ORACLE!', 'L') AS INSTR_1,
+	INSTR('HELLO, ORACLE!','L', 5) AS INSTR_2,
+	INSTR('HELLO, ORACLE!','L',2,2) AS INSTR_3
+FROM DUAL;
+
+-- 사원 이름에 S가 있는 행 구하기
+-- LIKE
+SELECT *
+FROM EMP e 
+WHERE E.ENAME LIKE '%S%';
+-- INSTR
+SELECT *
+FROM EMP e 
+WHERE INSTR(ENAME,'S')> 0; 
+
+-- 010-1234-1535
+SELECT '010-1234-1535' AS REPLACE_BEFORE,
+REPLACE('010-1234-1535','-',' ')AS REPLACE_1,
+REPLACE('010-1234-1535','-')AS REPLACE_2 
+FROM 
+DUAL;
+
+-- EMPNO,ENAME 연결 조회
+-- CONCAT(EMPNO,CONCAT(':',ENAME))
+SELECT CONCAT(EMPNO,ENAME),CONCAT(EMPNO,CONCAT(':',ENAME)) 
+FROM EMP e ;
+
+-- || == CONCAT
+SELECT EMPNO || ENAME ,EMPNO || ':'|| ENAME 
+FROM EMP e 
+
+SELECT
+	'[' || TRIM(' _Oracle_ ') || ']' AS TRIM,
+	'[' || LTRIM(' _Oracle_ ') || ']' AS LTRIM,
+	'[' || LTRIM('<_Oracle_>','_<') || ']' AS LTRIM2,
+	'[' || RTRIM(' _Oracle_ ') || ']' AS RTRIM,
+	'[' || RTRIM(' <_Oracle_>','>_') || ']' AS RTRIM2
+	FROM DUAL;
+
+--숫자함수
+-- ROUND(숫자,[반올림위치])
+-- TRUNC(숫자,[버림위치])
+-- CEIL(숫자) : 지정한 숫자와 가장 가까운 큰 정수 찾기
+-- FLOOR(숫자) : 지정한 숫자와 가장 가까운 작은 정수 찾기
+-- MOD(숫자, 나눌숫자) == %
+
+SELECT
+	ROUND(1234.5678) AS ROUND,
+	ROUND(1234.5678,0) AS ROUND0,
+	ROUND(1234.5678,1) AS ROUND_1,
+	ROUND(1234.5678,2) AS ROUND_2,
+	ROUND(1234.5678,-1) AS ROUND_MINUS1,
+	ROUND(1234.5678,-2) AS ROUND_MINUS2
+FROM
+DUAL;
+
+SELECT
+	TRUNC(1234.5678) AS TRUNC,
+	TRUNC(1234.5678,0) AS TRUNC0,
+	TRUNC(1234.5678,1) AS TRUNC_1,
+	TRUNC(1234.5678,2) AS TRUNC_2,
+	TRUNC(1234.5678,-1) AS TRUNC_MINUS1,
+	TRUNC(1234.5678,-2) AS TRUNC_MINUS2
+FROM
+DUAL;
+
+SELECT CEIL(3.14),FLOOR(3.14),CEIL(-3.14),FLOOR(-3.14) 
+FROM DUAL;
+
+SELECT MOD(15,6)
+FROM DUAL;
+
+-- 날짜함수
+-- SYSDATE : 오라클 서버가 설치된 os 의 현재날짜와 시간 사용
+-- 날짜데이터 + 숫자 : 날짜 데이터와 숫자만큼 일수 이후의 날짜
+-- 날짜데이터 - 날짜데이터 : 일수차이
+-- 날짜데이터 + 날짜데이터 : 안됨
+-- ADD_MONTHS(날짜데이터, 더할 개월수)
+-- MONTHS_BETWEEN(날짜데이터, 날짜데이터) : 두 날짜 데이터 간의 차이를 개월 수로 계산
+-- NEXT_DAY(날짜데이터,요일문자) : 날짜 데이터에서 돌아오는 요일의 날짜 반환
+-- LAST_DAY (날짜데이터) : 특정 날짜가 속한 마지막 날짜 조회
+
+SELECT SYSDATE FROM DUAL;
+
+SELECT SYSDATE,SYSDATE-1,SYSDATE+1 FROM DUAL;
+
+SELECT SYSDATE, ADD_MONTHS(SYSDATE,3) FROM DUAL; 
+
+-- 입사 20주년 조회
+SELECT
+	ADD_MONTHS(HIREDATE, 240),
+	E.ENAME ,
+	E.EMPNO ,
+	E.HIREDATE
+FROM
+	EMP e;
+
+SELECT 
+EMPNO ,
+ENAME ,
+HIREDATE ,
+MONTHS_BETWEEN(HIREDATE,SYSDATE) AS MONTH1, 
+MONTHS_BETWEEN(SYSDATE,HIREDATE) AS MONTH2,
+TRUNC(MONTHS_BETWEEN(SYSDATE,HIREDATE)) AS MONTH3
+FROM EMP;
+
+SELECT
+	SYSDATE,
+	NEXT_DAY(SYSDATE, '월요일'),
+	LAST_DAY(SYSDATE)
+FROM
+	DUAL;
+
+-- 형변환 함수
+-- TO_CHAR(날짜데이터, '출력되길 원하는 문자형태')
+-- TO_NUMBER(문자데이터,'인식 되길 원하는 숫자형태')
+-- TO_DATE(문자데이터,'인식되길 원하는 날짜형태')
+
+-- NUMBER + '문자숫자' : 연산해줌
+SELECT E.EMPNO ,E.ENAME ,E.EMPNO +'500'
+FROM EMP e;
+
+-- 수치가 부적합합니다
+-- SELECT E.EMPNO ,E.ENAME ,E.EMPNO +'abcd'
+-- FROM EMP e;
+
+-- 날짜데이터 => 문자데이터 
+-- 자바 월 mm / 분 MM
+-- 오라클 월 MM/ 분 MI
+SELECT SYSDATE ,TO_CHAR(SYSDATE,'YYYY/MM/DD') AS 현재날짜
+FROM DUAL;
+
+SELECT TO_CHAR(SYSDATE,'MM') AS 현재월,TO_CHAR(SYSDATE,'MON') AS 현재월2,TO_CHAR(SYSDATE,'MONTH') AS 현재월3
+FROM DUAL;
+
+-- DD, DDD, DAY
+SELECT TO_CHAR(SYSDATE,'DD') AS 일자, TO_CHAR(SYSDATE,'DAY') AS 일자2
+FROM DUAL;
+
+SELECT
+	SYSDATE,
+	TO_CHAR(SYSDATE, 'HH:MI:SS') AS 현재시간,
+	TO_CHAR(SYSDATE, 'HH12:MI:SS') AS 현재시간2,
+	TO_CHAR(SYSDATE, 'HH24:MI:SS') AS 현재시간3,
+	TO_CHAR(SYSDATE,'HH:MI:SS PM') AS 현재시간4
+FROM
+	DUAL;
+
+-- 문자데이터 => 숫자데이터 
+SELECT 1300 - '1500','1300' + 1500
+FROM DUAL;
+
+SELECT '1300' + '1500'
+FROM DUAL;
+
+--SELECT '1,300' + '1,500'
+--FROM DUAL;
+
+-- 9(숫자 한자리를 의미:빈자리를 채우지 않음) OR 0(숫자 한자리를 의미:빈자리를 채움)
+SELECT TO_NUMBER('1,300','999,999') + TO_NUMBER('1,500','999,999')
+FROM DUAL;
+
+-- 문자데이터 => 날짜데이터
+SELECT
+	TO_DATE('2024-09-05', 'YYYY-MM-DD') AS TODATE1,
+	TO_DATE('20240905', 'YYYY-MM-DD') AS TODATE2
+FROM
+	DUAL;
+
+-- EMP 테이블에서 1981-06-01 이후에 입사한 사원 조회
+SELECT *
+FROM EMP E
+WHERE E.HIREDATE >= '1981-06-01';
+
+SELECT *
+FROM EMP E
+WHERE E.HIREDATE >= TO_DATE('1981-06-01','YYYY-MM-DD');
+
+-- 날짜데이터 + 날짜데이터
+-- 수치가 부적합합니다
+-- SELECT '2024-09-05' - '2024-01-02'
+-- FROM DUAL
+
+-- 날짜와 날짜의 가산은 할 수 없습니다
+--SELECT TO_DATE('2024-09-05') + TO_DATE('2024-01-02')
+--FROM DUAL;
+
+SELECT TO_DATE('2024-09-05') - TO_DATE('2024-01-02')
+FROM DUAL;
+
+
+-- NULL 처리 함수 
+-- NULL 과 산술연산이 안됨 => NULL을 다른 값으로 변경
+-- NVL(널값,대체할값)
+-- NVL2(널값,널이 아닌경우 반환값,널인경우반환값)
+-- SAL = NULL (X)  SAL IS NULL(O)
+
+SELECT E.EMPNO ,E.ENAME ,E.SAL ,E.COMM ,E.SAL + NVL(COMM,0) 
+FROM EMP e;
+
+-- 널이 안니면 SAL * 12 + COMM
+-- 널이면 SAL *12
+SELECT E.EMPNO ,E.ENAME ,E.SAL ,E.COMM ,NVL2(COMM,'O','X'),NVL2(COMM,SAL*12+COMM,SAL*12) 
+FROM EMP e;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
