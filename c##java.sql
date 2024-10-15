ae48e217173c64f9158e24e50999dfb88f64470e
박현서 <qkr2625@gmail.com>
2024-10-15 17:55:58 +0900

    20241015 db문작성

--- c##java.sql
+++ c##java.sql
@@ -30,4 +30,82 @@ SELECT * FROM USERTBL u ;
 -- 아이디와 비밀번호 일치시 삭제
 DELETE USERTBL WHERE USERID = 'hong123' AND PASSWORD = 'hong789';
 
+-- booktbl
+-- code number(4) pk
+-- title 텍스트(50)
+-- writer 텍스트 (50)
+-- price number(10) 
+
+CREATE TABLE BOOKTBL(
+	CODE NUMBER(4) PRIMARY KEY,
+	TITLE VARCHAR2(50) NOT NULL,
+	WRITER VARCHAR2(50) NOT NULL,
+	PRICE NUMBER(10) NOT NULL
+);
+
+-- 1000 자바의 정석 신용균 25000
+-- 1001 자바의 신 강신용 29000
+-- 1002 자바 1000제 남궁성 32000
+-- 1003 오라클 박응용 33000
+-- 1004 점프투파이썬 신기성 35000
+
+INSERT INTO BOOKTBL VALUES(1000, '자바의 정석', '신용균', 25000);
+INSERT INTO BOOKTBL VALUES(1001, '자바의 신', '강신용', 29000);
+INSERT INTO BOOKTBL VALUES(1002, '자바 1000제', '남궁성', 32000);
+INSERT INTO BOOKTBL VALUES(1003, '오라클', '박응용', 33000);
+INSERT INTO BOOKTBL VALUES(1004, '점프투파이썬', '신기성', 35000);
+
+-- 전체 조회
+SELECT * FROM BOOKTBL;
+
+
+-- 도서번호 1000 번인 도서 조회(상세조회)
+SELECT * FROM BOOKTBL WHERE CODE = 1000;
+
+-- 도서번호 1001 번인 도서 가격 수정
+UPDATE
+	BOOKTBL
+SET
+	PRICE = 34000
+WHERE
+	CODE = 1001;
+-- 도서번호 1001 번인 도서 가격 및 상세 설명 수정
+UPDATE
+	BOOKTBL
+SET
+	PRICE = 34000
+	,
+	DESCRIPTION = '상세설명'
+WHERE
+	CODE = 1001;
+-- 도서번호 1004 번 도서 삭제
+DELETE FROM  BOOKTBL WHERE CODE = 1004;
+
+-- 도서명 '자바' 키워드가 들어있는 도서 조회
+SELECT * FROM BOOKTBL WHERE TITLE LIKE '%자바%';
+
+ALTER TABLE BOOKTBL ADD DESCRIPTION VARCHAR2(1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
